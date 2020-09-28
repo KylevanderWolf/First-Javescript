7e4d045e7f51d3ebd4293e9236c8967ed7aaca0e
KylevanderWolf <68204507+KylevanderWolf@users.noreply.github.com>
2020-09-28 14:13:33 +0200

    First-Javascript

---
+++ Javascript_1/script.js
@@ -0,0 +1,11 @@
+let name = 'Kyle'
+let numbers = 2 + 2
+let strings = "4" + "4"
+let formula1 = (2 * 2) + (400 - 800)
+let formula2 = (2 / 2) + (4 ^ 2)
+let formula3 = (299 / 2) + (4 + -1)
+let formula4 = 7 % 5
+let ageNumber = 30
+let ageString = "30"
+
+console.log(name, numbers, strings, formula1, formula2, formula3, formula4, ageNumber, ageString);
